54d0832b8ed1fa5248cf389adfcc0b740c446b7f
Ajay <ajay125545@gmail.com>
2026-04-26 11:23:43 +0530

    Add changes in first file

---
+++ season_1.py
@@ -0,0 +1,41 @@
+""" Write a program to take two numbers and print:sum,difference,product """
+
+num_1=23
+num_2=12
+print(num_1+num_2)
+
+
+""" Write a program to find area of rectangle. """
+
+l=23
+b=2
+Area=l*b
+print(Area)
+
+""" Write a program to check if a number is greater than 10. """
+
+num=24
+print(num>=10)
+
+""" Write a program to calculate: (10 + 5) * 2 - 3 ** 2 """
+calculation=(10 + 5) * 2 - 3 ** 2
+print(calculation)
+
+""" Find and fix error:
+name = Ajay
+print(name) """
+
+name="Ajay"
+print(name)
+
+""" Fix the error:
+print("Hello) """
+
+print("Hello")
+
+"""Output:
+print(5 + 2 * 3 ** 2)"""
+
+print(5 + 2 * 3 ** 2) # 23
+
+print(5.0 // 2)
